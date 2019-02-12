1eb18e0ce405228bcb00daed6b55b07001eb850b
Anthony Lopez <anthony.ron.lopez@gmail.com>
2019-02-12 15:15:04 -0600

    add prefabs and text file

---
+++ Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WordGenerator : MonoBehaviour {
+
+    public List<string> Words;
+    private string word; // file is new-line separated already
+    //private List<string> Words;
+
+    void Start () {
+        using (StreamReader sr = new StreamReader("Assets/Files/words.txt"))
+        {
+            while (!(sr.EndOfStream))
+            {
+                word = sr.ReadLine();
+                Words.Add(word);
+            }
+        }
+
+        foreach(string word in Words)
+        {
+            Debug.Log(word);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
