0cbf3ae05f14de19e1e53c07cf0f8b42c44f3062
Anthony Lopez <anthony.ron.lopez@gmail.com>
2019-02-13 22:48:19 -0600

    add basic word spawning mechanics
    At this point, the project is just a scene set up to create a Text object with a random word at a random position on-screen every second.

--- Assets/Scripts/WordGenerator.cs
+++ Assets/Scripts/WordGenerator.cs
@@ -2,15 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class WordGenerator : MonoBehaviour {
 
     public List<string> Words;
-    private string word; // file is new-line separated already
-    //private List<string> Words;
+    public Canvas maincanvas;
+    public GameObject WordText;
+    
+    public float LeftEdge = 100;
+    public float RightEdge = Screen.width - 100;
+    public float BottomEdge = 30;
+    public float TopEdge = Screen.height - 30;
 
     void Start () {
+        string word;
         using (StreamReader sr = new StreamReader("Assets/Files/words.txt"))
         {
             while (!(sr.EndOfStream))
@@ -20,14 +27,27 @@ public class WordGenerator : MonoBehaviour {
             }
         }
 
-        foreach(string word in Words)
-        {
-            Debug.Log(word);
-        }
+        LeftEdge = 100;
+        RightEdge = Screen.width - 100;
+        BottomEdge = 30;
+        TopEdge = Screen.height - 30;
+
+    InvokeRepeating("CreateText", 0.01f, 1.0f);
     }
 	
-	// Update is called once per frame
-	void Update () {
-		
+    void CreateText()
+    {
+        GameObject FallingWord = Instantiate(WordText, maincanvas.transform, true);
+        Text wordtext = FallingWord.GetComponent<Text>();
+
+        Vector2 rndPosition = new Vector2(Random.Range(LeftEdge, RightEdge), Random.Range(BottomEdge, TopEdge));
+        FallingWord.transform.position = rndPosition;
+
+        int rndIndex = Random.Range(0, Words.Count - 1);
+        wordtext.text = Words[rndIndex];
+    }
+
+    // Update is called once per frame
+    void Update () {
 	}
 }
